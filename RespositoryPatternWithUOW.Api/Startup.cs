4ce22e6a455ff445cbf1df1a00dc35fb059f44de
mina radi <majisa96@outlook.com>
2024-03-31 19:59:36 +0200

    +

--- RespositoryPatternWithUOW.Api/Startup.cs
+++ RespositoryPatternWithUOW.Api/Startup.cs
@@ -79,7 +79,6 @@ namespace RepositoryPatternWithUOW.Api
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-               // options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             })
             .AddJwtBearer(options =>
             {
