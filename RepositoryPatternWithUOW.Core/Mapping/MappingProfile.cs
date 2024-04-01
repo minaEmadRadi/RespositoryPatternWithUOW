efce0057316c80401bc3724935ab94a960f115a4
Mina Emad Radi <minaemad393@gmail.com>
2024-04-01 19:37:49 +0000

    +

--- RepositoryPatternWithUOW.Core/Mapping/MappingProfile.cs
+++ RepositoryPatternWithUOW.Core/Mapping/MappingProfile.cs
@@ -7,15 +7,11 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
 
-        CreateMap<ItemDto, Item>()/*.ForMember(dest => dest.Id, opt => {
+        CreateMap<ItemDto, Item>().ForMember(dest => dest.Id, 
+        opt => {
             opt.Condition(src => src.Id > 0);
             opt.MapFrom(src => src.Id);
-            })*/
-            .ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.ItemName)).ReverseMap();
-            /*.ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
-            .ForMember(dest => dest.UomId, opt => opt.MapFrom(src => src.UomId))
-            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
-            .ForMember(dest => dest.QTY, opt => opt.MapFrom(src => src.Qty))*/; 
+        }).ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.ItemName)).ReverseMap(); 
 
     }
 }
